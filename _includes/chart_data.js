ce7b42f760a3ea0fbc31076dc1cc7801933d2328
ZackDev <bastian.maier@gmx.de>
2020-03-21 15:39:47 +0100

    added infectionrate dataset

--- _includes/chart_data.js
+++ _includes/chart_data.js
@@ -1,4 +1,40 @@
-<script>
+<script type="text/javascript">
   var dates = ['27.1.2020','28.1.2020','29.1.2020','30.1.2020','31.1.2020','1.2.2020','2.2.2020','3.2.2020','4.2.2020','5.2.2020','6.2.2020','7.2.2020','8.2.2020','9.2.2020','10.2.2020','11.2.2020','12.2.2020','13.2.2020','14.2.2020','15.2.2020','16.2.2020','17.2.2020','18.2.2020','19.2.2020','20.2.2020','21.2.2020','22.2.2020','23.2.2020','24.2.2020','25.2.2020','26.2.2020','27.2.2020','28.2.2020','29.2.2020','1.3.2020','2.3.2020','3.3.2020','4.3.2020','5.3.2020','6.3.2020','7.3.2020','8.3.2020','9.3.2020','10.3.2020','11.3.2020','12.3.2020','13.3.2020','14.3.2020','15.3.2020','16.3.2020','17.3.2020','18.3.2020','19.3.2020','20.3.2020']
   var cases = [1,4,4,4,5,8,10,12,12,12,12,13,13,14,14,16,16,16,16,16,16,16,16,16,16,16,16,16,16,17,27,46,48,79,130,159,196,262,482,670,799,1040,1176,1457,1908,2078,3675,4585,5795,7272,9257,12327,15320,19848]
+  var rates = []
+  var average = 0
+  (function (c, r, a) {
+    var last_data = 0;
+    var sum_rates;
+    c.forEach(function(n){
+      if (n > 0 && last_data > 0){
+        r.append(last_data/data);
+        last_data = data;
+      } else if (n > 0){
+        last_data = data;
+      }
+    });
+    r.forEach(function(n){
+      sum_rates = sum_rates + n;
+    });
+    average = rates.length;
+  }(cases, rates, average));
+
+
+  def infection_rate(dataset):
+      last_data = 0
+      rates = []
+      average = 0
+      sum_rates = 0
+      for data in dataset:
+          if data > 0 and last_data > 0:
+              rates.append(data/last_data)
+              last_data = data
+          elif data > 0:
+              last_data = data
+      for r in rates:
+          sum_rates += r
+      print("{} - {}".format(len(dataset), len(rates)))
+      average = sum_rates/len(rates)
+      print(average)
 </script>
