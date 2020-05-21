ca6ca55387d6060501a8d9a36e121a889a35081b
ZackDev <bastian.maier@gmx.de>
2020-05-21 18:17:57 +0200

    updated ping output

--- assets/javascript/ping.js
+++ assets/javascript/ping.js
@@ -1,7 +1,7 @@
 var http_request = new XMLHttpRequest();
 
 function init(interval, target_url, target_div){
-  target_div.html('999ms');
+  target_div.html('ping:    ms');
   setInterval(ping, interval, target_url, target_div);
 }
 
