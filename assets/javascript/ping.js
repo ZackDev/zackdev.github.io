9a80c8df15addc000890daddb10be2b098ed9c2e
ZackDev <bastian.maier@gmx.de>
2020-05-21 13:50:01 +0200

    removed unused code, updated ping interval

--- assets/javascript/ping.js
+++ assets/javascript/ping.js
@@ -1,4 +1,3 @@
-var my_JSON_object;
 var http_request = new XMLHttpRequest();
 
 function init(interval, target_url, target_div){
@@ -12,15 +11,8 @@ function ping(target_url, target_div){
   http_request.onreadystatechange = function () {
     var done = 4, ok = 200;
     if (http_request.readyState === done && http_request.status === ok) {
-      my_JSON_object = http_request.response;
       let time_elapsed = Date.now() - now_ms;
       target_div.html(time_elapsed + 'ms');
-      console.log(Date.now() - now_ms);
-      console.log(http_request.readyState + " " + http_request.status);
-      console.log(my_JSON_object);
-    }
-    else {
-      console.log(http_request.readyState + " " + http_request.status);
     }
   };
   const now_ms = Date.now();
