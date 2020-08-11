39c8d2a97d0be60aab817e3e2e09773c22ce295b
ZackDev <bastian.maier@gmx.de>
2020-08-11 12:34:37 +0200

    added the weekly test cases chart

--- assets/javascript/corona.js
+++ assets/javascript/corona.js
@@ -1,16 +1,37 @@
-var http_request = new XMLHttpRequest();
-http_request.open("GET", "/assets/json/corona_germany_daily_cases.json", true);
-http_request.responseType = "json"
-http_request.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {
-    var cases = this.response.cases;
-    var dates = this.response.dates;
-    draw_chart(cases, dates);
-  }
+function init() {
+  init_daily_cases();
+  init_weekly_tests();
 };
-http_request.send(null);
 
-function draw_chart(cases, dates) {
+function init_daily_cases() {
+  var http_request = new XMLHttpRequest();
+  http_request.open("GET", "/assets/json/corona_germany_daily_cases.json", true);
+  http_request.responseType = "json"
+  http_request.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var cases = this.response.cases;
+      var dates = this.response.dates;
+      draw_daily_cases_chart(cases, dates);
+    }
+  };
+  http_request.send(null);
+};
+
+function init_weekly_tests() {
+  var http_request = new XMLHttpRequest();
+  http_request.open("GET", "/assets/json/corona_germany_weekly_tests.json", true);
+  http_request.responseType = "json"
+  http_request.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var weekly_tests = this.response.weekly_tests;
+      var calendar_weeks = this.response.calendar_weeks;
+      draw_weekly_tests_chart(weekly_tests, calendar_weeks);
+    }
+  };
+  http_request.send(null);
+};
+
+function draw_daily_cases_chart(cases, dates) {
   var daily_cases = [1];
   (function (c, d) {
     var last_data = 0;
@@ -27,7 +48,7 @@ function draw_chart(cases, dates) {
   Chart.defaults.global.defaultFontFamily = "'Myriad Pro', 'Calibri', Helvetica, sans-serif";
   Chart.defaults.global.defaultFontSize = 16;
 
-  var cg = document.getElementById('chart_corona_germany').getContext('2d');
+  var cg = document.getElementById('chart_corona_cases_germany').getContext('2d');
   var total_infections_chart = new Chart(cg, {
     type: 'line',
     data: {
@@ -84,4 +105,50 @@ function draw_chart(cases, dates) {
       }
     }
   });
-}
+};
+
+function draw_weekly_tests_chart(weekly_tests, calendar_weeks) {
+
+  Chart.defaults.global.defaultFontFamily = "'Myriad Pro', 'Calibri', Helvetica, sans-serif";
+  Chart.defaults.global.defaultFontSize = 16;
+
+  var cg = document.getElementById('chart_corona_tests_germany').getContext('2d');
+  var total_infections_chart = new Chart(cg, {
+    type: 'line',
+    data: {
+      labels: calendar_weeks,
+      datasets: [{
+        label: 'weekly tests',
+        backgroundColor: 'rgb(00, 144, 255)',
+        borderColor: 'rgb(00, 144, 255)',
+        data: weekly_tests,
+        fill: false,
+        yAxisID: 'weekly-tests-y-axis'
+      }]
+    },
+    options: {
+      scales: {
+        xAxes: [{
+          gridLines: {
+            display: false
+          }
+        }],
+        yAxes: [{
+          id: 'weekly-tests-y-axis',
+          type: 'linear',
+          position: 'right',
+          gridLines: {
+            display: false
+          },
+          scaleLabel: {
+            display: true,
+            labelString: 'weekly tests',
+            fontColor: 'rgb(00, 144, 255)'
+          }
+        }]
+      }
+    }
+  });
+};
+
+init();
--- pages/corona-chart-for-germany.md
+++ pages/corona-chart-for-germany.md
@@ -6,5 +6,10 @@ published: true
 list: true
 ---
 The following chart uses [chart.js](https://www.chartjs.org/) to display the total number (blue line) of confirmed SARS-CoV-2 cases for Germany. The daily increase (black bars) shows the day to day increase in cases.
+
+The second chart is showing the weekly performed tests for the SARS-CoV-2 virus. The displayed data lags behind two days, because they get officially published on the week's following Wednesdays.
+
 {% include corona.html %}
-The total daily cases in the chart comes from the [COVID-19 Data Repository by the Center for Systems Science and Engineering (CSSE) at Johns Hopkins University](https://github.com/CSSEGISandData/COVID-19) repository. The day to day increase in cases is a calculation on that data.
+
+The total daily cases in the first chart is taken from the [COVID-19 Data Repository by the Center for Systems Science and Engineering (CSSE) at Johns Hopkins University](https://github.com/CSSEGISandData/COVID-19) repository. The day to day increase in cases is a calculation on that data.
+The second chart's data comes from [RKI - Situations Report](https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Situationsberichte/2020-08-05-de.pdf?__blob=publicationFile).
