c9ed04b8d63420efff9adaa3140695add5313512
ZackDev <bastian.maier@gmx.de>
2020-08-12 15:29:59 +0200

    refactoring

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,11 +1,11 @@
 /* reusable javascript @ zackdev.github.io */
-const get_async_json = function get_async_json(url, callback) {
+const get_async_json = function get_async_json(url, callback_function) {
   let http_request = new XMLHttpRequest();
   http_request.open("GET", url, true);
   http_request.responseType = "json"
   http_request.onreadystatechange = function() {
     if (this.readyState === 4 && this.status === 200) {
-      callback(this);
+      callback_function(this);
     }
   };
   http_request.send(null);
