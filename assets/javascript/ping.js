e3ffd4e8d7e42b49f86107dbbefc713db1d73300
ZackDev <bastian.maier@gmx.de>
2020-12-10 18:43:13 +0100

    added console output for http status codes not 400

--- assets/javascript/ping.js
+++ assets/javascript/ping.js
@@ -7,14 +7,19 @@ function init_ping(interval, target_url, target_div){
 function ping(target_url, target_div){
   var http_request = new XMLHttpRequest();
   target_url += '?r=' + Date.now(); /* prevent json file from getting cached */
-  http_request.open("GET", target_url, true);
   http_request.responseType = "json";
   http_request.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      let time_elapsed = Date.now() - now_ms;
-      target_div.html('ping: ' + time_elapsed + 'ms');
+    if (this.readyState == 4) {
+      if (this.status == 200) {
+        let time_elapsed = Date.now() - now_ms;
+        target_div.html('ping: ' + time_elapsed + 'ms');
+      }
+      else {
+        console.log('ping.js: request completed with status code: ' + this.status);
+      }
     }
   };
+  http_request.open("GET", target_url, true);
   const now_ms = Date.now();
   http_request.send(null);
 };
