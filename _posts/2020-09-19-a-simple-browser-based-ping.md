944e2d7e94778090088978e5f66e26fa895c0aa4
ZackDev <bastian.maier@gmx.de>
2021-04-19 12:29:15 +0200

    upgraded ping to use a chart

--- _posts/2020-09-19-a-simple-browser-based-ping.md
+++ _posts/2020-09-19-a-simple-browser-based-ping.md
@@ -8,6 +8,6 @@ archived: true
 ---
 Since the well-known ping is not available at browser level, the script on this page retrieves a sample json file from the webserver. The time elapsed from sending the request to receiving the sample is displayed below.
 
-{% include ping.html %}
+{% include chart.html name="ping_chart" %}
 
 {% include javascript.html src="/assets/js/ping.js" %}
--- assets/js/ping.js
+++ assets/js/ping.js
@@ -1,10 +1,9 @@
-function run_ping(interval, target_url, target_div){
-  target_div.innerHTML = '';
-  ping(target_url, target_div)
-  setInterval(ping, interval, target_url, target_div);
+function run_ping(interval, target_url, dataset){
+  ping(target_url, dataset)
+  setInterval(ping, interval, target_url, dataset);
 };
 
-function ping(target_url, target_div){
+function ping(target_url, dataset){
   var http_request = new XMLHttpRequest();
   target_url += '?r=' + Date.now(); /* prevent json file from getting cached */
   http_request.responseType = "json";
@@ -12,7 +11,7 @@ function ping(target_url, target_div){
     if (this.readyState == 4) {
       if (this.status == 200) {
         let time_elapsed = Date.now() - now_ms;
-        target_div.innerHTML = 'ping: ' + time_elapsed + 'ms';
+        dataset.addPoint(time_elapsed, true, false);
       }
       else {
         console.log('ping.js: request completed with status code: ' + this.status);
@@ -24,12 +23,42 @@ function ping(target_url, target_div){
   http_request.send(null);
 };
 
+const init_chart = (interval, target_url) => {
+  let chart = Highcharts.chart('ping_chart', {
+      chart: {
+        type: 'line'
+      },
+      title: {
+        text: 'Round Trip Time'
+      },
+      subtitle: {
+        text: `updated every ${interval/1000} seconds`
+      },
+      xAxis: [{
+        crosshair: true,
+        allowDecimals: false
+      }],
+      yAxis: [{
+        title: {
+          text: 'Milliseconds'
+        },
+        crosshair: true
+      }],
+      series: [{
+        name: 'Ping In Milliseconds'
+      }],
+      credits: {
+        enabled: false
+      }
+  });
+  run_ping( interval, target_url, chart.series[0]);
+}
+
+
 const init_ping = function init_ping() {
   let interval = 3000;
   let target_url = "/assets/json/dummy.json";
-  let target_div = document.getElementById("ping_output");
-
-  run_ping(interval, target_url, target_div);
+  init_chart(interval, target_url);
 };
 
 new Init(init_ping);
