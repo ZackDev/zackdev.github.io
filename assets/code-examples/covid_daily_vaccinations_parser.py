8c965fdafc10fb1ff8635acab0ed24e2e442d34e
ZackDev <bastian.maier@gmx.de>
2021-06-02 16:15:33 +0200

    added updated parser

--- assets/code-examples/covid_daily_vaccinations_parser.py
+++ assets/code-examples/covid_daily_vaccinations_parser.py
@@ -2,86 +2,78 @@ import argparse
 import csv
 import json
 import math
+from openpyxl import load_workbook
 
-def run(inputfile, outputfile):
-    with open(inputfile, newline='') as daily_vaccinations_csv:
-        dates_raw = []
-        primary_vaccinations_raw = []
-        secondary_vaccinations_raw = []
-        dates = []
-        primary_vaccinations = []
-        secondary_vaccinations = []
-        dict = None
+def run(excelfile, outputfile):
+    # open .xslx file, set working sheet to 'Impfungen_proTag'
+    # select columns B C and D
+    # clear direct formatting
+    wb = load_workbook(filename=excelfile)
+    if wb.sheetnames.count('Impfungen_proTag') != 1:
+        exit(1)
 
-        ''' NOTE: the first line contains the header '''
-        ''' each line after that corresponds to a date '''
-        csv_reader = csv.reader(daily_vaccinations_csv, delimiter=',', dialect='excel')
-        index = 0
-        for line in csv_reader:
-            if index > 0:
-                raw_date = line[0]
-                primary_raw_vaccination = line[1]
-                secondary_raw_vaccination = line[2]
-                if raw_date is not None and primary_raw_vaccination is not None and secondary_raw_vaccination is not None:
-                    arr_raw_date = raw_date.split('/')
-                    if raw_date != '' and len(arr_raw_date) == 3:
-                        dates_raw.append(raw_date)
-                        primary_vaccinations_raw.append(primary_raw_vaccination)
-                        secondary_vaccinations_raw.append(secondary_raw_vaccination)
-            index+=1
+    wb.active = wb['Impfungen_proTag']
+    ws = wb.active
 
-        if dates_raw and primary_vaccinations_raw and secondary_vaccinations_raw:
-            dates = []
-            primary_vaccinations = []
-            secondary_vaccinations = []
+    dates = []
+    primary_vaccinations = []
+    secondary_vaccinations = []
 
-            ''' do some date parsing, provided format is M/D/Y, to YYYY-MM-DD '''
-            for raw_date in dates_raw:
-                date_array = raw_date.split('/')
-                day = int(date_array[1])
-                month = int(date_array[0])
-                year = int(date_array[2])
-                if day < 10:
-                    day = f'0{day}'
-                if month < 10:
-                    month = f'0{month}'
-                dates.append(f"{year}-{month}-{day}")
+    row_index = 0
+    for row in ws:
+        # first row is header row and doesn't contain usefull values
+        if row_index > 0:
+            col_index = 0
+            row_value = str(row[0].value)
+            # parse rows until end of interesting data
+            if (( row_value is None ) or (row_value == '') or (row_value == 'None')):
+                break
+            for col in row:
+                # the dates column
+                if col_index == 0:
+                    raw_date = str(col.value)
+                    date = raw_date.split(' ')[0]
+                    dates.append(date)
+                # the primary vaccinations column
+                elif col_index == 1:
+                    raw_p_vacc = str(col.value)
+                    if raw_p_vacc.isnumeric():
+                        primary_vaccinations.append(int(raw_p_vacc))
+                # the secondary vaccinations column
+                elif col_index == 2:
+                    raw_s_vacc = str(col.value)
+                    if raw_s_vacc.isnumeric():
+                        secondary_vaccinations.append(int(raw_s_vacc))
+                    elif raw_s_vacc is None or raw_s_vacc == 'None':
+                        secondary_vaccinations.append(int(0))
+                col_index +=1
+        row_index +=1
 
-            ''' do simple string to integer conversion '''
-            for raw_vaccination in primary_vaccinations_raw:
-                vaccination = math.floor(float(raw_vaccination))
-                primary_vaccinations.append(vaccination)
+    # write csvfile
+    ''' check data for consistency, equal amount of dates and cases '''
+    if len(dates) != len(primary_vaccinations) != len(secondary_vaccinations):
+        print('length mismatch. ending programm.')
+        print(f'dates: {len(dates)} primary_vaccinations: {len(primary_vaccinations)} secondary_vaccinations: {len(secondary_vaccinations)}')
+        exit(1)
 
-            for raw_vaccination in secondary_vaccinations_raw:
-                if raw_vaccination == '':
-                    raw_vaccination = 0
-                vaccination = math.floor(float(raw_vaccination))
-                secondary_vaccinations.append(vaccination)
+    if len(dates) < 1 and len(primary_vaccinations) < 1 and len(secondary_vaccinations):
+        print('no data extracted. ending programm.')
+        exit(1)
 
+    dict = { 'dates':dates, 'primary_vaccinations':primary_vaccinations, 'secondary_vaccinations':secondary_vaccinations }
+    with open(outputfile ,'w') as output:
+        output.write(json.dumps(dict))
 
-        ''' check data for consistency, equal amount of dates and cases '''
-        if len(dates) != len(primary_vaccinations) != len(secondary_vaccinations):
-            print('length mismatch. ending programm.')
-            print(f'dates: {len(dates)} primary_vaccinations: {len(primary_vaccinations)} secondary_vaccinations: {len(secondary_vaccinations)}')
-            return
-
-        if len(dates) < 1 and len(primary_vaccinations) < 1 and len(secondary_vaccinations):
-            print('no data extracted. ending programm.')
-            return
-
-        dict = { 'dates':dates, 'primary_vaccinations':primary_vaccinations, 'secondary_vaccinations':secondary_vaccinations }
-        with open(outputfile ,'w') as output:
-            output.write(json.dumps(dict))
-
-        print('wrote dict to file. program finished.')
-
+    print('wrote dict to file. program finished.')
+    exit(0)
 
 if __name__ == '__main__':
     arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument("-i", "--inputfile", type=str)
+    arg_parser.add_argument("-x", "--excelfile", type=str)
     arg_parser.add_argument("-o", "--outputfile", type=str)
     args = arg_parser.parse_args()
-    if (args.inputfile and args.outputfile):
-        run(args.inputfile, args.outputfile)
+    if (args.excelfile and args.outputfile):
+        run(args.excelfile, args.outputfile)
     else:
         arg_parser.print_help()
+        exit(0)
--- assets/code-examples/covid_weekly_tests_parser.py
+++ assets/code-examples/covid_weekly_tests_parser.py
@@ -1,73 +1,74 @@
 import argparse
 import csv
 import json
+from openpyxl import load_workbook
 
-def run(inputfile, outputfile):
-    with open(inputfile, newline='') as weekly_tests_csv:
-        calendar_weeks = []
-        calendar_weeks_raw = []
-        weekly_tests = []
-        weekly_tests_raw = []
-        dict = None
+def run(excelfile, outputfile):
 
-        for x in range(1, 10):
-            weekly_tests.append(0)
-            calendar_weeks_raw.append(f'{x}/2020')
+    wb = load_workbook(filename=excelfile)
+    if wb.sheetnames.count('1_Testzahlerfassung') != 1:
+        exit(1)
+    wb.active = wb['1_Testzahlerfassung']
+    ws = wb.active
 
-        ''' NOTE: the interesting data starts from line 4 '''
-        csv_reader = csv.reader(weekly_tests_csv, delimiter=',', dialect='excel')
-        index = 0
-        for line in csv_reader:
-            raw_test = None
-            raw_date = None
-            if index == 1:
-                raw_week = '10/2020'
-                raw_test = line[2]
-            elif index >= 2:
-                raw_week = line[0]
-                raw_test = line[1]
-            if raw_test is not None and raw_week is not None and raw_test != '' and raw_week != '' and raw_week != 'Summe':
-                weekly_tests_raw.append(raw_test)
-                calendar_weeks_raw.append(raw_week)
-            index+=1
+    calendar_weeks = []
+    weekly_tests = []
 
-        ''' do some calendar week conversion '''
-        for raw_week in calendar_weeks_raw:
-            week = int(raw_week.split('/')[0])
-            year = int(raw_week.split('/')[1].replace('*',''))
-            if week < 10:
-                week = f'0{week}'
-            week = f'{year}-W{week}'
-            calendar_weeks.append(week)
+    for x in range(1, 10):
+        calendar_weeks.append(f'2020-W0{x}')
+        weekly_tests.append(0)
 
-        ''' conversion from string to integer '''
-        for raw_test in weekly_tests_raw:
-            test = int(raw_test)
-            weekly_tests.append(test)
+    row_index = 0
+    for row in ws:
+        if row_index == 1:
+            calendar_weeks.append(f'2020-W10')
+            weekly_tests.append(int(row[2].value))
+        elif row_index >= 2:
+            if row[0].value == 'Summe' or row[0].value is None:
+                break
+            col_index = 0
+            for col in row:
+                if col_index == 0:
+                    raw_calendar_week = str(col.value)
+                    raw_week = int(raw_calendar_week.split('/')[0])
+                    year = int(raw_calendar_week.split('/')[1])
+                    if raw_week < 10:
+                        week = f'0{raw_week}'
+                    else:
+                        week = raw_week
+                    calendar_week = f'{year}-W{week}'
+                    calendar_weeks.append(calendar_week)
 
-        ''' data consistency check, length calendar_weeks equals length weekly_tests '''
-        if len(calendar_weeks) != len(weekly_tests):
-            print('length mismatch. ending programm.')
-            print(f'calendar_weeks: {len(calendar_weeks)} weekly tests: {len(weekly_tests)}')
-            return
+                elif col_index == 1:
+                    tests = int(col.value)
+                    weekly_tests.append(tests)
+                col_index +=1
+        row_index +=1
 
-        if len(calendar_weeks) < 1 and len(weekly_tests) < 1:
-            print('no data extracted. ending programm.')
-            return
+    ''' data consistency check, length calendar_weeks equals length weekly_tests '''
+    if len(calendar_weeks) != len(weekly_tests):
+        print('length mismatch. ending programm.')
+        print(f'calendar_weeks: {len(calendar_weeks)} weekly tests: {len(weekly_tests)}')
+        exit(1)
 
-        dict = {'calendar_weeks':calendar_weeks, 'weekly_tests':weekly_tests}
-        with open(outputfile ,'w') as output:
-            output.write(json.dumps(dict))
+    if len(calendar_weeks) < 1 and len(weekly_tests) < 1:
+        print('no data extracted. ending programm.')
+        exit(1)
 
-        print('wrote dict to file. program finished.')
+    dict = {'calendar_weeks':calendar_weeks, 'weekly_tests':weekly_tests}
+    with open(outputfile ,'w') as output:
+        output.write(json.dumps(dict))
+
+    print('wrote dict to file. program finished.')
 
 
 if __name__ == '__main__':
     arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument("-i", "--inputfile", type=str)
+    arg_parser.add_argument("-x", "--excelfile", type=str)
     arg_parser.add_argument("-o", "--outputfile", type=str)
     args = arg_parser.parse_args()
-    if (args.inputfile and args.outputfile):
-        run(args.inputfile, args.outputfile)
+    if (args.excelfile and args.outputfile):
+        run(args.excelfile, args.outputfile)
     else:
         arg_parser.print_help()
+        exit(1)
