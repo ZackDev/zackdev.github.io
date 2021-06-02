731b22b4e279fbecb0c57c55f691da09bd742e26
ZackDev <bastian.maier@gmx.de>
2021-06-02 20:38:09 +0200

    added updated daily_cases parser

--- assets/code-examples/covid_daily_cases_parser.py
+++ assets/code-examples/covid_daily_cases_parser.py
@@ -38,30 +38,57 @@ def run(inputfile, outputfile):
             ''' do some date parsing, provided format is M/D/Y, to YYYY-MM-DD '''
             for raw_date in raw_dates:
                 date_array = raw_date.split('/')
-                day = int(date_array[1])
-                month = int(date_array[0])
-                year = int(date_array[2])
-                if day < 10:
+                day, month, year = None, None, None
+                if len(date_array) == 3:
+                    if date_array[0].isnumeric() and date_array[1].isnumeric() and date_array[2].isnumeric():
+                        day = int(date_array[1])
+                        month = int(date_array[0])
+                        year = int(date_array[2])
+                    else:
+                        print('error: parsing day, month and year. trying to parse non-numeric values.')
+                        exit(1)
+                else:
+                    print('error: parsing raw_date. length != 3')
+                    exit(1)
+                if day >= 1 and day < 10:
                     day = f'0{day}'
-                if month < 10:
+                elif day >=10 and day <= 31:
+                    day = f'{day}'
+                else:
+                    print('error: day not in range 1...31')
+                    exit(1)
+                if month >= 1 and month < 10:
                     month = f'0{month}'
+                elif month >= 10 and month <= 12:
+                    month = f'{month}'
+                else:
+                    print('error: month not in range ')
+                    exit(1)
                 year = f'20{year}'
                 dates.append(f"{year}-{month}-{day}")
 
             ''' simple string to integer conversion '''
             for raw_case in raw_cases:
-                case = int(raw_case)
-                cases.append(case)
+                if raw_case.isnumeric():
+                    case = int(raw_case)
+                    if case >= 0:
+                        cases.append(case)
+                    else:
+                        print('error: case is negative.')
+                        exit(1)
+                else:
+                    print('error: case is not numeric')
+                    exit(1)
 
         ''' check data for consistency, equal amount of dates and cases '''
         if len(dates) != len(cases):
             print('length mismatch. ending programm.')
             print(f'dates: {len(dates)} cases: {len(cases)}')
-            return
+            exit(1)
 
         if len(dates) < 1 and len(cases) < 1:
             print('no data extracted. ending programm.')
-            return
+            exit(1)
 
         dict = { 'dates':dates, 'cases':cases}
         with open(outputfile ,'w') as output:
@@ -81,3 +108,4 @@ if __name__ == '__main__':
         run(args.inputfile, args.outputfile)
     else:
         arg_parser.print_help()
+        exit(1)
