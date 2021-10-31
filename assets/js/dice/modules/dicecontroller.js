05c68e40ce43c9f63ae99dbd2226b2c471778ef2
ZackDev <bastian.maier@gmx.de>
2021-10-31 18:55:03 +0100

    added comments for the DiceController

--- assets/js/dice/modules/dicecontroller.js
+++ assets/js/dice/modules/dicecontroller.js
@@ -16,32 +16,77 @@ export {DiceController};
         this.diceProvider = new DiceProvider(this);
         this.bucket = new Bucket(this);
     }
+    /**
+     * toggles the NewDiceTypeView's visibility
+     */
     onAddNewDiceTypeClicked() {
+        // called by the DiceTypesView
         this.newDiceTypeView.toggleVisibility();
     }
+    /**
+     * creates a new dice from the provided dice type UID and adds it to the Bucket
+     * @param {number} UID the UID of the dice type used to create the dice 
+     */
     onDiceTypeClicked(UID) {
+        // called by the DiceTypesView
         let dice = this.diceProvider.createDice(UID);
         this.bucket.addDice(dice);
     }
+    /**
+     * adds a new dice type to the DiceProvider
+     * @param {string} name 
+     * @param {Array<string>} sides 
+     */
     onCreateDiceClicked(name, sides) {
+        // called by the NewDiceTypeView
         this.diceProvider.addDiceType(name, sides);
     }
+    /**
+     * adds a dice type to the DiceTypesView
+     * @param {number} UID the UID of the dice type
+     * @param {string} name the name of the dice type
+     */
     onDiceTypeAdded(UID, name) {
+        // called by the NewDiceTypesView
         this.diceTypesView.displayDiceType(UID, name);
     }
+    /**
+     * removes a Dice from the Bucket
+     * @param {number} UID the UID of the dice to remove
+     */
     removeDiceFromBucket(UID) {
+        // called by the BucketView
         this.bucket.removeDice(UID);
     }
+    /**
+     * adds a dice to the BucketView
+     * @param {Dice} dice the dice added to the bucket
+     */
     onDiceAddedToBucket(dice) {
+        // called by the DiceTypesView
         this.bucketView.displayDice(dice.UID, dice.name);
     }
+    /**
+     * removes a dice from the BucketView
+     * @param {number} UID the UID of the to remove
+     */
     onDiceRemovedFromBucket(UID) {
+        // called by the Bucket
         this.bucketView.removeDice(UID);
     }
+    /**
+     * rolls all the Dices in the Bucket
+     */
     onRollClicked() {
+        // called by the BucketView
         this.bucket.roll();
     }
+    /**
+     * adds a dice to the TableView
+     * @param {Dice} dice the dice to add
+     */
     onDiceRolled(dice) {
+        // called by the BucketView
         this.tableView.displayDice(dice.UID, dice.name, dice.result);
     }
 }
