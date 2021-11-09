424a0330500fb66a37c76ed9c4096f279c0b1e67
ZackDev <ZackDev@users.noreply.github.com>
2021-11-09 11:28:27 +0100

    changed 'ping.js' typing to camelCase

--- assets/js/ping.js
+++ assets/js/ping.js
@@ -1,29 +1,29 @@
-const run_ping = (interval, target_url, dataset) => {
-  ping(target_url, dataset)
-  setInterval(ping, interval, target_url, dataset);
+const runPing = (interval, targetUrl, dataset) => {
+  ping(targetUrl, dataset)
+  setInterval(ping, interval, targetUrl, dataset);
 };
 
-const ping = (target_url, dataset) => {
-  let http_request = new XMLHttpRequest();
-  target_url += '?r=' + Date.now(); /* prevent json file from getting cached */
-  http_request.responseType = "json";
-  http_request.onreadystatechange = function () {
+const ping = (targetUrl, dataset) => {
+  let httpRequest = new XMLHttpRequest();
+  targetUrl += '?r=' + Date.now(); /* prevent json file from getting cached */
+  httpRequest.responseType = "json";
+  httpRequest.onreadystatechange = function () {
     if (this.readyState == 4) {
       if (this.status == 200) {
-        let time_elapsed = Date.now() - now_ms;
-        dataset.addPoint(time_elapsed, true, false);
+        let timeElapsed = Date.now() - nowMs;
+        dataset.addPoint(timeElapsed, true, false);
       }
       else {
         console.log('ping.js: request completed with status code: ' + this.status);
       }
     }
   };
-  http_request.open("GET", target_url, true);
-  let now_ms = Date.now();
-  http_request.send(null);
+  httpRequest.open("GET", targetUrl, true);
+  let nowMs = Date.now();
+  httpRequest.send(null);
 };
 
-const init_chart = (interval) => {
+const initChart = (interval) => {
   return Highcharts.chart('ping-chart', {
       chart: {
         type: 'line'
@@ -54,11 +54,11 @@ const init_chart = (interval) => {
 }
 
 
-const init_ping = () => {
+const initPing = () => {
   let interval = 3000;
-  let target_url = "/assets/json/dummy.json";
-  let chart = init_chart(interval);
-  run_ping( interval, target_url, chart.series[0]);
+  let targetUrl = "/assets/json/dummy.json";
+  let chart = initChart(interval);
+  runPing( interval, targetUrl, chart.series[0]);
 };
 
-new Init(init_ping);
+new Init(initPing);
