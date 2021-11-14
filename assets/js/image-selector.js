0ebdc9fb9c6e49549e74b18df2638b7fb9f4391f
ZackDev <ZackDev@users.noreply.github.com>
2021-11-14 19:46:25 +0100

    rearranged comments

--- assets/js/image-selector.js
+++ assets/js/image-selector.js
@@ -14,33 +14,39 @@ class Album {
             this.root = albumContainer;
             document.body.append(this.root);
         }
-        // create HTML-Elements containing:
-        // - container for the selected image
-        // - the selected image
-        // - preview container, holding smaller preview versions of the images in the album
+        // create HTML-Elements:
+        // the container for the selected image
         let mainImageContainer = document.createElement("div");
         mainImageContainer.id = "album-main-image-container";
+
+        // the selected image
         let mainImage = document.createElement("img");
         mainImage.id = "album-main-image";
         mainImage.addEventListener("load", () => {
-            // set height style to trigger transformation
+            // set height style of it's container to trigger transformation
             let height = this.mainImage.height;
             this.mainImageContainer.style.height = `${height}px`;
         });
-        mainImageContainer.append(mainImage);
+        // the preview container, holding smaller preview versions of the images in the album
         let imagesPreviewContainer = document.createElement("div");
         imagesPreviewContainer.id = "album-preview-container";
+        mainImageContainer.append(mainImage);
+
+        // add them to the DOM
         this.root.append(mainImageContainer);
         this.root.append(imagesPreviewContainer);
+
+        // set references
         this.mainImageContainer = mainImageContainer;
         this.mainImage = mainImage;
         this.imagesPreviewContainer = imagesPreviewContainer;
     }
 
     addImage(url) {
-        // add the image url to the album and create a preview element for it
-        // preselects the first image added to the album
+        // add the image url to the album
         let index = this.images.push(url) -1;
+        
+        // create a preview element for it
         let previewImage = document.createElement("img");
         previewImage.classList.add("album-preview-image");
         previewImage.id = `album-preview-image-${index}`;
@@ -50,26 +56,31 @@ class Album {
         });
         this.imagesPreviewContainer.append(previewImage);
         if (this.images.length === 1) {
+            // preselect the first image added to the album
             this.changeSelectedImageByIndex(0);
         }
     }
 
     changeSelectedImageByIndex(i) {
-        // - highlights the selected preview image
-        // - de-highlights the previously preview image
-        // - changes the src attribute of the fully displayed image
+        // de-highlight the selected preview image
         if (this.selectedImageIndex !== undefined) {
-            // removed "selected" image class from previoulsy selected image
-            let pI = document.getElementById(`album-preview-image-${this.selectedImageIndex}`);
-            pI.classList.add("album-preview-image");
-            pI.classList.remove("album-preview-selected-image");
+            if (pI !== null) {
+                let pI = document.getElementById(`album-preview-image-${this.selectedImageIndex}`);
+                pI.classList.add("album-preview-image");
+                pI.classList.remove("album-preview-selected-image");
+            }
         }
-        this.selectedImageIndex = i;
-        let s = document.getElementById(`album-preview-image-${i}`);
-        s.classList.add("album-preview-selected-image");
-        s.classList.remove("album-preview-image");
-        s.removeEventListener("click", this.changeSelectedImageByIndex);
 
-        this.mainImage.src = this.images[this.selectedImageIndex];
+        // highlight the previously preview image
+        let s = document.getElementById(`album-preview-image-${i}`);
+        if (s !== null) {
+            s.classList.add("album-preview-selected-image");
+            s.classList.remove("album-preview-image");
+            s.removeEventListener("click", this.changeSelectedImageByIndex, i);
+        }
+
+        // change the main image
+        this.mainImage.src = this.images[i];
+        this.selectedImageIndex = i;
     }
 }
