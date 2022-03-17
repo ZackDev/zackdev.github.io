cd932c2caed16c6bd9e2cbe8662343b6b1a70c90
ZackDev <ZackDev@users.noreply.github.com>
2022-03-17 11:42:43 +0100

    album.js: added counter displaying images loaded in relation to the total amount of images in the album

--- assets/js/album.js
+++ assets/js/album.js
@@ -18,11 +18,22 @@ class Album {
 
         // array holding the album's images' urls
         this.images = [];
-
+        this.imagesLoaded = 0;
         // for storing the index of the selected image
         this.selectedImageIndex = undefined;
 
         // create HTML-Elements:
+        // - the container holding the album's stats
+        let albumStatsContainer = document.createElement("div");
+        albumStatsContainer.id = "album-stats-container";
+        albumStatsContainer.classList.add("flex-row");
+        albumStatsContainer.style.justifyContent = "space-around";
+
+        let albumStats = document.createElement("div");
+        albumStats.id = "album-stats";
+
+        albumStatsContainer.append(albumStats);
+
         // - the container for the selected image
         let mainImageContainer = document.createElement("div");
         mainImageContainer.id = "album-main-image-container";
@@ -52,10 +63,13 @@ class Album {
         imagesPreviewContainer.id = "album-preview-container";
 
         // add them to the DOM
+        this.root.append(albumStatsContainer);
         this.root.append(mainImageContainer);
         this.root.append(imagesPreviewContainer);
 
         // set references
+        this.albumStatsContainer = albumStatsContainer;
+        this.albumStats = albumStats;
         this.mainImageContainer = mainImageContainer;
         this.mainImage = mainImage;
         this.imagesPreviewContainer = imagesPreviewContainer;
@@ -66,6 +80,7 @@ class Album {
     addImage(url) {
         // add the image url to the images array
         const index = this.images.push(url) -1;
+        this.updateAlbumStats();
         
         // create a preview element
         const previewImage = document.createElement("img");
@@ -74,6 +89,8 @@ class Album {
         previewImage.src = url;
         previewImage.decode()
             .then(() => {
+                this.imagesLoaded += 1;
+                this.updateAlbumStats();
                 previewImage.style.opacity = "0";
                 this.imagesPreviewContainer.append(previewImage);
                 // triggers the 'fade-in' animation for the preview image
@@ -117,5 +134,9 @@ class Album {
         // - update the selected image index
         this.mainImageContainer.style.opacity = "0";
         this.selectedImageIndex = i;
+    }
+
+    updateAlbumStats() {
+        this.albumStats.innerHTML = `${this.imagesLoaded} images out of ${this.images.length} loaded.`;
     }
 }
