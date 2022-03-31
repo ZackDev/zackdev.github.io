1a88445b6c8429b1a360e3b5cc3a273c2ccb7d28
ZackDev <ZackDev@users.noreply.github.com>
2022-03-31 15:25:12 +0200

    dice.js: reworked NewDiceTypeView UI

--- assets/js/dice/modules/views/newdicetypeview.js
+++ assets/js/dice/modules/views/newdicetypeview.js
@@ -23,31 +23,65 @@ export { NewDiceTypeView };
             // an array for adding and removing the dice's sides
             this.diceSides = [];
             let label = document.createElement("label");
-            label.innerHTML = "create a new type of dice."
-            // the input for setting the dice's name
+            label.innerHTML = "create a new type of dice.";
+
+            // the name wrap
+            let nameWrap = document.createElement("div");
+            nameWrap.classList.add("flex-column");
+            // - the info element
+            let nameInfoWrap = document.createElement("div");
+            nameInfoWrap.classList.add("flex-row");
+            nameInfoWrap.style.justifyContent = "space-between";
+            let nameInfoText = document.createElement("div");
+            nameInfoText.innerText = "name: 1 to 4 characters";
+            let nameInfoCheckOrX = document.createElement("div");
+            nameInfoCheckOrX.id = "create-dice-type-name-check-or-x";
+            nameInfoCheckOrX.innerHTML = "&cross;";
+            nameInfoWrap.append(nameInfoText, nameInfoCheckOrX);
+            // - the input for setting the dice's name
             let nameInput = document.createElement("input");
             nameInput.id = "dice-name-input";
             nameInput.type = "text";
-            nameInput.placeholder = "enter a name here."
+            nameInput.placeholder = "enter a name here.";
             nameInput.minLength = 1;
             nameInput.maxLength = 4;
-            // the output for displaying the dice's sides
-            let sideOutput = document.createElement("div");
-            sideOutput.id = "sides-container";
-            sideOutput.classList.add("flex-row");
-            // the input for adding sides
+            nameInput.addEventListener("input", () => {
+                this.adaptNameInfo();
+            });
+            // - add the input's elements to the wrapper
+            nameWrap.append(nameInfoWrap);
+            nameWrap.append(nameInput);
+
+            // the side wrap
+            let sideWrap = document.createElement("div");
+            sideWrap.classList.add("flex-column");
+            // - the sides input info
+            let sidesInfoWrap = document.createElement("div");
+            sidesInfoWrap.classList.add("flex-row");
+            sidesInfoWrap.style.justifyContent = "space-between";
+            let sidesInfoText = document.createElement("div");
+            sidesInfoText.innerText = "sides: more than one side.";
+            let sidesInfoCheckOrX = document.createElement("div");
+            sidesInfoCheckOrX.id = "create-dice-type-sides-check-or-x";
+            sidesInfoCheckOrX.innerHTML = "&cross;";
+            // - the wrapper for the side input
+            let sideInputWrap = document.createElement("div");
+            sideInputWrap.classList.add("flex-row");
+            sideInputWrap.style.justifyContent = "space-between";
+            // - the side input for entering a side
             let sideInput = document.createElement("input");
             sideInput.id = "dice-sides-input";
             sideInput.type = "text";
-            sideInput.placeholder = "enter a side value here."
+            sideInput.placeholder = "enter a side value here.";
             sideInput.minLength = 1;
             sideInput.maxLength = 1;
-            // the container holding buttons:
-            let btnContainer = document.createElement("div");
-            btnContainer.id = "btn-container";
+            sideInput.addEventListener("input", () => {
+                this.adaptAddSideButton();
+            });
             // - the 'addSideBtn'
             // adds the value of this.sideInput to this.sides
             let addSideBtn = document.createElement("button");
+            addSideBtn.classList.add("unclickable");
             addSideBtn.innerHTML = "add side";
             addSideBtn.addEventListener("click", () => {
                 let sideValue = this.sideInput.value;
@@ -65,42 +99,57 @@ export { NewDiceTypeView };
                         if (i > -1) {
                             this.diceSides.splice(i, 1);
                             side.remove();
+                            this.adaptSidesInfo();
                         }
                     });
                     this.sideOutput.append(side);
                     this.diceSides.push(sideValue);
                     this.sideInput.value = "";
                     this.sideInput.focus();
-                } 
+                    this.adaptSidesInfo();
+                    this.adaptAddSideButton();
+                }
             });
+            sidesInfoWrap.append(sidesInfoText, sidesInfoCheckOrX);
+            sideInputWrap.append(sideInput);
+            sideInputWrap.append(addSideBtn);
+            // - the output for displaying the dice's sides
+            let sideOutput = document.createElement("div");
+            sideOutput.id = "sides-container";
+            sideOutput.classList.add("flex-row");
+            
+            sideWrap.append(sidesInfoWrap);
+            sideWrap.append(sideInputWrap);
+            sideWrap.append(sideOutput);
+
             // - the 'createDiceBtn', calls the controllers onCreateDiceClicked() method
             // - with this.nameInput and this.dicesSides as parameters
             let createDiceBtn = document.createElement("button");
+            createDiceBtn.classList.add("unclickable");
             createDiceBtn.innerHTML = "create dice";
             createDiceBtn.addEventListener("click", () => {
                 // only create 'valid' dices
                 // - with minimum two sides
                 // - and the length of the name bewtween one and four
-                if (this.diceSides.length >= 2 && this.nameInput.value.length >= 1 && this.nameInput.value.length <= 4 ) {
+                if (this.diceSides.length >= 2 && this.nameInput.value.length >= 1 && this.nameInput.value.length <= 4 && this.nameInput.value.length >= 1 ) {
                     this.controller.onCreateDiceTypeClicked(this.nameInput.value, this.diceSides);
                     this.clear();
+                    this.adaptNameInfo();
+                    this.adaptSidesInfo();
+                    this.hide();
                 }
             });
-            // - add the buttons to the button container
-            btnContainer.append(addSideBtn);
-            btnContainer.append(createDiceBtn);
             // add the relevant UI elements to this for future referece
             this.nameInput = nameInput;
-            this.sideOutput = sideOutput;
             this.sideInput = sideInput;
+            this.sideOutput = sideOutput;
             this.addSideBtn = addSideBtn;
             this.createDiceBtn = createDiceBtn;
-            // add the created HTML-Elements to the view's container
+             // add the created HTML-Elements to the view's container
             this.root.append(label);
-            this.root.append(nameInput);
-            this.root.append(sideOutput);
-            this.root.append(sideInput);
-            this.root.append(btnContainer);
+            this.root.append(nameWrap);
+            this.root.append(sideWrap);
+            this.root.append(createDiceBtn);
             this.hide();
             this.controller = controller;
 
@@ -120,7 +169,54 @@ export { NewDiceTypeView };
         this.sideInput.value = "";
         this.diceSides = [];
     }
-    
+
+    adaptNameInfo() {
+        let lenName = this.nameInput.value.length;
+        if (lenName >= 1 && lenName <= 4) {
+            document.getElementById("create-dice-type-name-check-or-x").innerHTML = "&check;";
+        }
+        else {
+            document.getElementById("create-dice-type-name-check-or-x").innerHTML = "&cross;";
+        }
+        this.adaptCreateButton();
+    }
+
+    adaptSidesInfo() {
+        let numSides = this.diceSides.length;
+        if (numSides >= 2) {
+            document.getElementById("create-dice-type-sides-check-or-x").innerHTML = "&check;";
+        }
+        else {
+            document.getElementById("create-dice-type-sides-check-or-x").innerHTML = "&cross;";
+        }
+        this.adaptCreateButton();
+    }
+
+    adaptCreateButton() {
+        let lenName = this.nameInput.value.length;
+        let numSides = this.diceSides.length;
+        if (lenName >= 1 && lenName <=4 && numSides >=2) {
+            this.createDiceBtn.classList.remove("unclickable");
+            this.createDiceBtn.classList.add("clickable");
+        }
+        else {
+            this.createDiceBtn.classList.remove("clickable");
+            this.createDiceBtn.classList.add("unclickable");
+        }
+    }
+
+    adaptAddSideButton() {
+        let lenSideInput = this.sideInput.value.length;
+        if (lenSideInput >= 1 && lenSideInput <= 4) {
+            this.addSideBtn.classList.remove("unclickable");
+            this.addSideBtn.classList.add("clickable");
+        }
+        else {
+            this.addSideBtn.classList.remove("clickable");
+            this.addSideBtn.classList.add("unclickable");
+        }
+    }
+
     /**
      * toggle's the view's visibility
      */
