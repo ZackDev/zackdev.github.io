848b70b26e3a55aade35dcb2fed60a339a6ae119
ZackDev <ZackDev@users.noreply.github.com>
2022-05-03 16:58:09 +0200

    main: added pubsub prototype

---
+++ assets/js/pubsub.mjs
@@ -0,0 +1,48 @@
+const dispatcher = {
+    connector: new Map(),
+    deregister(subscriber, id) {
+        console.log();
+        if (dispatcher.connector.has(id)) {
+            let subscribers = dispatcher.connector.get(id);
+            if (subscribers.includes(subscriber)) {
+                let i = subscribers.indexOf(subscriber);
+                subscribers.splice(i, 1);
+            }
+        }
+    },
+    register(subscriber, id) {
+        if (dispatcher.connector.has(id)) {
+            let subscribers = dispatcher.connector.get(id);
+            subscribers.push(subscriber);
+        }
+        else {
+            dispatcher.connector.set(id, [subscriber]);
+        }
+    },
+    dispatch: (id, data) => {
+        if (dispatcher.connector.has(id)) {
+            let subscribers = dispatcher.connector.get(id);
+            for (let sub of subscribers) {
+                sub.push(data);
+            }
+        }
+    },
+}
+
+class Publisher {
+    publish(id, data) {
+        dispatcher.dispatch(id, data);
+    }
+}
+
+class Subscriber {
+    subscribe(id) {
+        dispatcher.register(this, id);
+    }
+    unsubscribe(id) {
+        dispatcher.deregister(this, id);
+    }
+    push(data) {
+        console.log(data);
+    }
+}
