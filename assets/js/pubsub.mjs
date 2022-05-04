28c48aa662956b2519c57cfcbdbf74c84ca8bd3d
ZackDev <ZackDev@users.noreply.github.com>
2022-05-04 11:25:33 +0200

    cleanup

--- assets/js/pubsub.mjs
+++ assets/js/pubsub.mjs
@@ -23,7 +23,7 @@ const dispatcher = {
         if (dispatcher.connector.has(id)) {
             let subscribers = dispatcher.connector.get(id);
             for (let sub of subscribers) {
-                sub.notify(data);
+                sub.notify(id, data);
             }
         }
     },
@@ -36,14 +36,20 @@ class Publisher {
 }
 
 class Subscriber {
-    subscribe(id) {
+    constructor() {
+        this.idToHook = new Map();
+    }
+    subscribe(id, hook) {
+        this.idToHook.set(id, hook);
         dispatcher.register(this, id);
     }
     unsubscribe(id) {
+        this.idToHook.del(id);
         dispatcher.deregister(this, id);
     }
-    notify(data) {
-        console.log(data);
+    notify(id, data) {
+        let hook = this.idToHook.get(id);
+        hook(data);
     }
 }
 
