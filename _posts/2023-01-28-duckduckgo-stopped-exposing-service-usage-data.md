ef21bc6a6e5a67ee47d9db965ec5e8b1e4b7d2de
ZackDev <ZackDev@users.noreply.github.com>
2023-01-28 11:44:23 +0100

    added ddg daily queries

---
+++ _posts/2023-01-28-duckduckgo-stopped-exposing-service-usage-data.md
@@ -0,0 +1,13 @@
+---
+title: duckduckgo stopped exposing service usage data
+tags: [web, search-engine]
+published: true
+---
+
+As of 2022-11-22, the search engine stopped providing usage data via https://duckduckgo.com/traffic_data/direct.csv. The historical data is charted below.
+
+<hr>
+
+<div id="chart-ddg-daily-queries"><div>
+
+<script type="module" src="/assets/js/ddg-daily-queries-chart.mjs" />
---
+++ assets/js/ddg-daily-queries-chart.mjs
@@ -0,0 +1,51 @@
+import { checkHTTPResponse, Init } from '/assets/js/main.mjs';
+
+/* eslint-disable no-console */
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-undef */
+const initDDGQueries = () => {
+    const dailyQueriesUrl = '/assets/json/daily_queries_ddg.json';
+
+    const fInit = {
+        method: 'GET',
+        cache: 'no-cache'
+    }
+
+    /** */
+    fetch(dailyQueriesUrl, fInit)
+        .then(
+            res => checkHTTPResponse(res),
+            rej => console.log(rej))
+        .then(
+            res => res.json(),
+            rej => console.log(rej)
+        )
+        .then(
+            res => drawDDGDailyQueriesChart(res),
+            rej => console.log(rej)
+        );
+}
+
+function drawDDGDailyQueriesChart(dataObj) {
+    Highcharts.chart('chart-ddg-daily-queries', {
+        chart: {
+            type: 'column',
+            zoomType: 'x',
+        },
+        title: {
+            text: 'DuckDuckGo Daily Queries',
+        },
+        xAxis: {
+            categories: dataObj.dates,
+        },
+        series: [{
+            name: "queries",
+            data: dataObj.queries,
+        }],
+        tooltip: {
+            shared: true,
+        }
+    });
+}
+
+new Init(initDDGQueries);
